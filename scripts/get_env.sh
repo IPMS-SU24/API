719f8366068a7a2eeec95399c0059dec6ef00dad
baominh <haphanbaominh9674@gmail.com>
2024-05-30 00:17:40 +0700

    add get_env.sh

---
+++ scripts/get_env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+AWS_REGION="ap-southeast-1"
+
+SECRET_NAME="env-demo"
+
+SECRET_VALUE=$(aws secretsmanager get-secret-value --region $AWS_REGION --secret-id $SECRET_NAME --query SecretString --output text)
+
+echo "SECRET_VALUE: $SECRET_VALUE"
+
+# Use process substitution instead of a pipe
+while IFS="=" read -r key value; do
+    # Trim whitespace from key and value
+    key=$(echo "$key" | awk '{$1=$1};1')
+    value=$(echo "$value" | awk '{$1=$1};1')
+    echo "$key - $value"
+    echo "export $key='$value'" >> ~/.bashrc
+    echo "export $key='$value'" >> ~/.bash_profile
+done < <(echo "$SECRET_VALUE" | jq -r 'to_entries[] | "\(.key)=\(.value)"')
+
+source ~/.bash_profile
