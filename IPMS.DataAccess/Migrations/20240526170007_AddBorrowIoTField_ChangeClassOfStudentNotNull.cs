1ee25fd5cc69b86ff7f7483090e01fed7657965d
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-05-29 21:16:02 +0700

    #024 - Migration Add borrow IoT field

---
+++ IPMS.DataAccess/Migrations/20240526170007_AddBorrowIoTField_ChangeClassOfStudentNotNull.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IPMS.DataAccess.Migrations
+{
+    public partial class AddBorrowIoTField_ChangeClassOfStudentNotNull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ClassId",
+                table: "Student",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BorrowIoTComponentDeadline",
+                table: "IPMSClass",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BorrowIoTComponentDeadline",
+                table: "IPMSClass");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ClassId",
+                table: "Student",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+        }
+    }
+}
