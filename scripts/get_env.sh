018252c8af61981859c030aee983caa4b174837d
baominh <haphanbaominh9674@gmail.com>
2024-05-30 00:26:18 +0700

    change secretsmanager name

--- scripts/get_env.sh
+++ scripts/get_env.sh
@@ -3,7 +3,7 @@ set -e
 
 AWS_REGION="ap-southeast-1"
 
-SECRET_NAME="env-demo"
+SECRET_NAME="env"
 
 SECRET_VALUE=$(aws secretsmanager get-secret-value --region $AWS_REGION --secret-id $SECRET_NAME --query SecretString --output text)
 
