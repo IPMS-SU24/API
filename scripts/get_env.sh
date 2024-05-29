3fb096eec9c06f95029c0638f8023eb5efb83649
baominh <haphanbaominh9674@gmail.com>
2024-05-30 00:59:56 +0700

    create script to create .env

--- scripts/get_env.sh
+++ scripts/get_env.sh
@@ -9,16 +9,6 @@ SECRET_VALUE=$(aws secretsmanager get-secret-value --region $AWS_REGION --secret
 
 echo "SECRET_VALUE: $SECRET_VALUE"
 
-# Use process substitution instead of a pipe
-while IFS="=" read -r key value; do
-    # Trim whitespace from key and value
-    key=$(echo "$key" | awk '{$1=$1};1')
-    value=$(echo "$value" | awk '{$1=$1};1')
-    echo "$key - $value"
-    echo "export $key='$value'" >> ~/.bashrc
-    echo "export $key='$value'" >> ~/.bash_profile
-done < <(echo "$SECRET_VALUE" | jq -r 'to_entries[] | "\(.key)=\(.value)"')
-
 source ~/.bash_profile
 
 # Save key-value pairs to .env file
