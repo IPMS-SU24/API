fddd4cb7daf23422da08ab1239e4fe28aeb89958
tan-phannhat-se <tan.phannhat.se@gmail.com>
2024-06-02 15:58:15 +0700

    #43 - update name presignedUrl

--- IPMS.Business/Interfaces/Services/IPresignedUrlService.cs
+++ IPMS.Business/Interfaces/Services/IPresignedUrlService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IPMS.Business.Requests.AWS;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,6 @@ namespace IPMS.Business.Interfaces.Services
     public interface IPresignedUrlService
     {
         public string GeneratePresignedDownloadUrl(string objectKey);
-        public string GeneratePresignedUploadUrl(string objectKey);
+        public string GeneratePresignedUploadUrl(string objectName);
     }
 }
