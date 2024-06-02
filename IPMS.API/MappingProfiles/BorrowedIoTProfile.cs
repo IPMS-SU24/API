f12ea7ecf215affb08ed1128a48502d355713f4e
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-06-02 13:56:49 +0700

    #040 - Fix borrow IoT API

---
+++ IPMS.API/MappingProfiles/BorrowedIoTProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using IPMS.Business.Responses.ProjectDashboard;
+using IPMS.DataAccess.Models;
+
+namespace IPMS.API.MappingProfiles
+{
+    public class BorrowedIoTProfile : Profile
+    {
+        public BorrowedIoTProfile()
+        {
+            CreateMap<ComponentsMaster, BorrowIoTComponentInformation>()
+                .ForMember(x => x.Id, opt => opt.MapFrom(src => src.ComponentId))
+                .ForMember(x => x.Name, opt => opt.MapFrom(src => src.Component!.Name));
+        }
+    }
+}
