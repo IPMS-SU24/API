c97b0e17b3c7afa86b4bd77df8a43b091f3a50d2
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-06-10 00:08:20 +0700

    #078 - Hot Fix Register Topic

--- IPMS.Business/Requests/Topic/RegisterTopicRequest.cs
+++ IPMS.Business/Requests/Topic/RegisterTopicRequest.cs
@@ -5,6 +5,7 @@ namespace IPMS.Business.Requests.Topic
     public class RegisterTopicRequest
     {
         public string TopicName { get; set; } = null!;
+        public string ShortName { get; set; } = null!;
         public string Description { get; set; } = null!;
         public string? FileName { get; set; }
         public ICollection<IoTModelRequest> IoTComponents { get; set; } = null!;
