6da00a8c3071f93338779e7165bab192a4d6a2e9
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-06-11 00:13:52 +0700

    #077 - Implement Join/Swap API

---
+++ IPMS.API/Validators/Group/SwapGroupRequestValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using IPMS.API.Common.Extensions;
+using IPMS.Business.Interfaces.Services;
+using IPMS.Business.Requests.Group;
+
+namespace IPMS.API.Validators.Group
+{
+    public class SwapGroupRequestValidator : AbstractValidator<SwapGroupRequest>
+    {
+        public SwapGroupRequestValidator(IStudentGroupService studentGroupService, IHttpContextAccessor context)
+        {
+            var studentId = context.HttpContext.User.Claims.GetUserId();
+            RuleFor(x => x).CustomAsync(async (x, validationContext, cancellationToken) =>
+            {
+                var validationResult = await studentGroupService.CheckValidRequestSwap(x,studentId);
+                if (!validationResult.Result)
+                {
+                    validationContext.AddFailure(validationResult.Message);
+                }
+            });
+        }
+    }
+}
