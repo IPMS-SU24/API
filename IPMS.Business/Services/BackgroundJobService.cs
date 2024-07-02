7b0e71032103e6233a1108e3ff2c390ccb3ba142
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-07-03 00:50:23 +0700

    #129 - hot fix import job

--- IPMS.Business/Services/BackgroundJobService.cs
+++ IPMS.Business/Services/BackgroundJobService.cs
@@ -33,75 +33,79 @@ namespace IPMS.Business.Services
         }
         [AutomaticRetry(Attempts = 5)]
         [DisableConcurrentExecution(timeoutInSeconds: 10 * 60)]
-        public async Task ProcessAddStudentToClass(StudentDataRow student, Guid classId, string serverDomain)
+        public async Task ProcessAddStudentToClass(List<StudentDataRow> students, Guid classId, string serverDomain)
         {
             await _unitOfWork.RollbackTransactionOnFailAsync(async () =>
             {
                 var existStudentInClass = await _unitOfWork.StudentRepository.Get().Where(x => x.ClassId == classId).ToListAsync();
-                await _unitOfWork.SaveChangesAsync();
                 if (existStudentInClass != null)
                 {
                     _unitOfWork.StudentRepository.HardDeleteRange(existStudentInClass);
                 }
-                //Check mail is exist
-                var existUser = await _userManager.FindByEmailAsync(student.Email);
-                //If not exist => create account
-                if (existUser == null)
+                await _unitOfWork.SaveChangesAsync();
+                foreach (var student in students)
                 {
-                    var password = PasswordGeneratorUtils.GenerateRandomPassword();
-                    var stuAccount = new IPMSUser
+                    
+                    //Check mail is exist
+                    var existUser = await _userManager.FindByEmailAsync(student.Email);
+                    //If not exist => create account
+                    if (existUser == null)
                     {
-                        FullName = student.StudentName,
-                        UserName = student.StudentId,
-                        Email = student.Email,
-                        SecurityStamp = Guid.NewGuid().ToString()
-
-                    };
-                    var result = await _userManager.CreateAsync(stuAccount, password);
-                    if (!result.Succeeded)
-                    {
-                        throw new CannotCreateAccountException();
-                    }
-                    existUser = await _userManager.FindByEmailAsync(student.Email);
-                    //Send mail confirm
-                    try
-                    {
-                        var confirmEmailToken = await _userManager.GenerateEmailConfirmationTokenAsync(existUser);
-                        var confirmURL = PathUtils.GetConfirmURL(serverDomain, existUser.Id, confirmEmailToken);
-                        EmailSendOperation emailSendOperation = await _mailServer.Client.SendAsync(
-                            WaitUntil.Completed,
-                            "DoNotReply@5c9fc577-26d6-49e3-98e1-62c04cc4e4e0.azurecomm.net",
-                            student.Email,
-                            ConfirmEmailTemplate.Subject,
-                            ConfirmEmailTemplate.GetBody(confirmURL, password));
-                        EmailSendResult statusMonitor = emailSendOperation.Value;
-
-                        if (statusMonitor.Status == EmailSendStatus.Failed)
+                        var password = PasswordGeneratorUtils.GenerateRandomPassword();
+                        var stuAccount = new IPMSUser
                         {
-                            throw new SendMailFailException();
+                            FullName = student.StudentName,
+                            UserName = student.StudentId,
+                            Email = student.Email,
+                            SecurityStamp = Guid.NewGuid().ToString()
+
+                        };
+                        var result = await _userManager.CreateAsync(stuAccount, password);
+                        if (!result.Succeeded)
+                        {
+                            throw new CannotCreateAccountException();
+                        }
+                        existUser = await _userManager.FindByEmailAsync(student.Email);
+                        //Send mail confirm
+                        try
+                        {
+                            var confirmEmailToken = await _userManager.GenerateEmailConfirmationTokenAsync(existUser);
+                            var confirmURL = PathUtils.GetConfirmURL(serverDomain, existUser.Id, confirmEmailToken);
+                            EmailSendOperation emailSendOperation = await _mailServer.Client.SendAsync(
+                                WaitUntil.Completed,
+                                "DoNotReply@5c9fc577-26d6-49e3-98e1-62c04cc4e4e0.azurecomm.net",
+                                student.Email,
+                                ConfirmEmailTemplate.Subject,
+                                ConfirmEmailTemplate.GetBody(confirmURL, password));
+                            EmailSendResult statusMonitor = emailSendOperation.Value;
+
+                            if (statusMonitor.Status == EmailSendStatus.Failed)
+                            {
+                                throw new SendMailFailException();
+                            }
+                        }
+                        catch (RequestFailedException ex)
+                        {
+                            throw new SendMailFailException(ex);
                         }
                     }
-                    catch (RequestFailedException ex)
+                    //If exist => create student info in class, send notification
+                    else
                     {
-                        throw new SendMailFailException(ex);
+                        await _messageService.SendMessage(new NotificationMessage()
+                        {
+                            AccountId = existUser.Id,
+                            Message = "You are added into new IOT102 Class",
+                            Title = "New Class Assigned"
+                        });
                     }
-                }
-                //If exist => create student info in class, send notification
-                else
-                {
-                    await _messageService.SendMessage(new NotificationMessage()
+                    await _unitOfWork.StudentRepository.InsertAsync(new Student
                     {
-                        AccountId = existUser.Id,
-                        Message = "You are added into new IOT102 Class",
-                        Title = "New Class Assigned"
+                        ClassId = classId,
+                        InformationId = existUser.Id
                     });
+                    await _unitOfWork.SaveChangesAsync();
                 }
-                await _unitOfWork.StudentRepository.InsertAsync(new Student
-                {
-                    ClassId = classId,
-                    InformationId = existUser.Id
-                });
-                await _unitOfWork.SaveChangesAsync();
             });
         }
     }
