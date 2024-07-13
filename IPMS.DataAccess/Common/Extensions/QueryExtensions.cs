2ec6fba9c51e663e379db1349a12db3921ceb0f0
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-07-13 16:27:06 +0700

    #140 - apply ensure soft delete work

--- IPMS.DataAccess/Common/Extensions/QueryExtensions.cs
+++ IPMS.DataAccess/Common/Extensions/QueryExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using IPMS.DataAccess.Common.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 
 namespace IPMS.DataAccess.Common.Extensions
@@ -9,34 +10,46 @@ namespace IPMS.DataAccess.Common.Extensions
         {
             return source.Where(x => x.IsDeleted);
         }
-        public static IQueryable<T> ApplySoftDeleteFilter<T>(this IQueryable<T> query) where T : BaseModel
+        public static IQueryable<T> ApplySoftDeleteFilter<T>(this IQueryable<T> query, IPMSDbContext context) where T : BaseModel
         {
             var entityType = typeof(T);
             var parameter = Expression.Parameter(entityType, "e");
 
             var body = Expression.AndAlso(
                 Expression.Equal(Expression.Property(parameter, nameof(BaseModel.IsDeleted)), Expression.Constant(false)),
-                GetRelatedEntitiesFilter(entityType, parameter)
+                GetRelatedEntitiesFilter(entityType, parameter, context)
             );
 
             var lambda = Expression.Lambda<Func<T, bool>>(body, parameter);
             return query.Where(lambda);
         }
 
-        private static Expression GetRelatedEntitiesFilter(Type entityType, ParameterExpression parameter)
+        private static Expression GetRelatedEntitiesFilter(Type entityType, ParameterExpression parameter, IPMSDbContext context)
         {
             Expression combinedExpression = Expression.Constant(true);
 
-            var properties = entityType.GetProperties();
-            foreach (var property in properties)
+            var entityTypeModel = context.Model.FindEntityType(entityType);
+            if (entityTypeModel == null) return combinedExpression;
+
+            var foreignKeys = entityTypeModel.GetForeignKeys();
+            foreach (var foreignKey in foreignKeys)
             {
-                if (typeof(BaseModel).IsAssignableFrom(property.PropertyType))
+                var navigation = foreignKey.DependentToPrincipal;
+                if (navigation != null)
                 {
-                    var relatedEntity = Expression.Property(parameter, property.Name);
+                    var relatedEntity = Expression.Property(parameter, navigation.Name);
                     var isDeletedProperty = Expression.Property(relatedEntity, nameof(BaseModel.IsDeleted));
                     var isNotDeleted = Expression.Equal(isDeletedProperty, Expression.Constant(false));
 
-                    combinedExpression = Expression.AndAlso(combinedExpression, isNotDeleted);
+                    var foreignKeyProperty = foreignKey.Properties.FirstOrDefault();
+                    if (foreignKeyProperty != null && Nullable.GetUnderlyingType(foreignKeyProperty.ClrType) != null)
+                    {
+                        var foreignKeyNotNull = Expression.NotEqual(Expression.Property(parameter, foreignKeyProperty.Name), Expression.Constant(null, foreignKeyProperty.ClrType));
+                        isNotDeleted = Expression.AndAlso(foreignKeyNotNull, isNotDeleted);
+                        var foreignKeyNull = Expression.Equal(Expression.Property(parameter, foreignKeyProperty.Name), Expression.Constant(null, foreignKeyProperty.ClrType));
+                        isNotDeleted = Expression.Or(foreignKeyNull, isNotDeleted);
+                        combinedExpression = Expression.AndAlso(combinedExpression, isNotDeleted);
+                    }
                 }
             }
 
