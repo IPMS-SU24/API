2ff25d9772e0125a867c32c150664fb2ef8c5dbe
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-07-21 21:24:29 +0700

    #173 - hot fix validator

--- IPMS.API/Validators/Assessment/GetAssessmentByIdValidator.cs
+++ IPMS.API/Validators/Assessment/GetAssessmentByIdValidator.cs
@@ -1,16 +1,17 @@
 ﻿using FluentValidation;
 using IPMS.Business.Interfaces.Services;
+using IPMS.Business.Requests.Assessment;
 
 namespace IPMS.API.Validators.Assessment
 {
-    public class GetAssessmentByIdValidator : AbstractValidator<Guid>
+    public class GetAssessmentByIdValidator : AbstractValidator<GetAssessmentByIdRequest>
     {
         public GetAssessmentByIdValidator(IAssessmentService topicService, IHttpContextAccessor context)
         {
             RuleFor(x => x).NotEmpty();
             RuleFor(x => x).CustomAsync(async (x, validationContext, cancellationToken) =>
             {
-                var validationResult = await topicService.GetAssessmentByIdValidators(x);
+                var validationResult = await topicService.GetAssessmentByIdValidators(x.AssessmentId);
                 if (!validationResult.Result)
                 {
                     validationContext.AddFailure(validationResult.Message);
