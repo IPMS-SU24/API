7c34b56f6089e0bb0f038fb2665cbab54888daa8
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-07-26 01:25:23 +0700

    #173 - add api get evaluate

---
+++ IPMS.API/Validators/Group/GetEvaluateMemberByLecturerValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using IPMS.API.Common.Extensions;
+using IPMS.Business.Interfaces.Services;
+using IPMS.Business.Requests.Group;
+
+namespace IPMS.API.Validators.Group
+{
+    public class GetEvaluateMemberByLecturerValidator : AbstractValidator<GetMemberContributionRequest>
+    {
+        public GetEvaluateMemberByLecturerValidator(IHttpContextAccessor context, IStudentGroupService studentGroupService)
+        {
+            var lecturerId = context.HttpContext.User.Claims.GetUserId();
+            RuleFor(x => x).CustomAsync(async (x, validationContext, cancellationToken) =>
+            {
+                var validationResult = await studentGroupService.CheckGetContributeByLecturer(x, lecturerId);
+                if (!validationResult.Result)
+                {
+                    validationContext.AddBusinessFailure(validationResult.Message);
+                }
+            });
+        }
+    }
+}
---
+++ IPMS.Business/Responses/Group/MemberEvaluateResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace IPMS.Business.Responses.Group
+{
+    public class MemberEvaluateResponse
+    {
+        public Guid MemberId { get; set; }
+        public decimal? LeaderSetPercentage { get; set; }
+        public decimal? LecturerSetPercentage { get; set; }
+        public string Name { get; set; }
+    }
+}
