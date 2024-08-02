8fb35344d87916b9eb10b6f28ae91dfcd08637d7
tan-phannhat-se <tan.phannhat.se@gmail.com>
2024-08-03 00:01:33 +0700

    #265 - add description to submission module

--- IPMS.Business/Responses/SubmissionModule/GetSubmissionModuleByClassResposne.cs
+++ IPMS.Business/Responses/SubmissionModule/GetSubmissionModuleByClassResposne.cs
@@ -26,5 +26,6 @@ namespace IPMS.Business.Responses.SubmissionModule
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public decimal Percentage { get; set; }
+        public string Description { get; set; }
     }
 }
