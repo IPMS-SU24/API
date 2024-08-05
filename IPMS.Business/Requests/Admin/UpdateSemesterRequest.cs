b167fea280b12d8bb38f17503211909b7d31bba3
tan-phannhat-se <tan.phannhat.se@gmail.com>
2024-08-06 00:41:26 +0700

    #268 - crud semester

---
+++ IPMS.Business/Requests/Admin/UpdateSemesterRequest.cs
@@ -0,0 +1,14 @@
+﻿
+namespace IPMS.Business.Requests.Admin
+{
+    public class UpdateSemesterRequest
+    {
+        public Guid? Id { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Guid SyllabusId { get; set; }
+    }
+}
