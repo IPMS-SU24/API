c6840b28030a687ec98a4ae3f6b160cf890a240d
Hà Phan Bảo Minh <73026297+HaPhanBaoMinh@users.noreply.github.com>
2024-08-19 21:06:31 +0700

    Update start_docker_image.sh

--- scripts/start_docker_image.sh
+++ scripts/start_docker_image.sh
@@ -3,22 +3,22 @@ AWS_REGION="ap-southeast-1"
 AWS_ACCOUNT_ID="905418022082"
 REPOSITORY_NAME="ipmg_api"
 
-# Thực hiện start Docker container từ Docker image đã được pull và map cổng 80 trên máy host với cổng 300 trên container
+# Start Docker container 
 
 # Tên của Docker container muốn khởi chạy
 DOCKER_IMAGE="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPOSITORY_NAME:latest"
 
-# Kiểm tra xem container đã tồn tại hay không
+# Check container is exist
 if [ "$(docker ps -a -q -f name=$REPOSITORY_NAME)" ]; then
-    # Nếu container tồn tại, thì stop và xóa container cũ
+    # If exist, delete docker container
     docker stop $REPOSITORY_NAME
     docker rm $REPOSITORY_NAME
 fi
 
-# Thực hiện start Docker container và map cổng 80 trên máy host với cổng 300 trên container
+# Start Docker container
 docker run -e TZ=Asia/Ho_Chi_Minh -d -e ASPNETCORE_URLS='http://+:443' --name $REPOSITORY_NAME -p 443:443 $DOCKER_IMAGE
 docker image prune -f
-# Kiểm tra xem container đã khởi chạy thành công hay không
+# Check docker container status
 if [ $? -eq 0 ]; then
   echo "Container đã được khởi chạy thành công với tên: $REPOSITORY_NAME và port 80 trên máy host đã được map với port 300 trên container."
 else
@@ -26,5 +26,5 @@ else
   exit 1
 fi
 
-# Kết thúc script
+# End script
 exit 0
