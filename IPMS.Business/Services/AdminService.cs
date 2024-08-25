644766cc75f049ec50b7735785f9e9c86e0bf9b5
tan-phannhat-se <tan.phannhat.se@gmail.com>
2024-08-25 15:14:00 +0700

    #323 - check syllabus has assessment

--- IPMS.Business/Services/AdminService.cs
+++ IPMS.Business/Services/AdminService.cs
@@ -544,13 +544,17 @@ namespace IPMS.Business.Services
                 }
             }
 
-            var syllabus = await _unitOfWork.SyllabusRepository.Get().FirstOrDefaultAsync(s => s.Id.Equals(request.SyllabusId));
+            var syllabus = await _unitOfWork.SyllabusRepository.Get().Where(s => s.Id.Equals(request.SyllabusId)).Include(s => s.Assessments).FirstOrDefaultAsync();
             if (syllabus == null)
             {
                 result.Message = "Syllabus is not existed";
                 return result;
             }
 
+            if (syllabus.Assessments.Count() == 0) {
+                result.Message = "Syllabus doesn't have any assessment";
+                return result;
+            }
             result.Message = string.Empty;
             result.Result = true;
             return result;
