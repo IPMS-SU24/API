c77ee28dadff96ec6790724856b00ca45e4c06b4
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-08-25 20:57:43 +0700

    #290 - fix repo

--- IPMS.Business/Requests/IoTComponent/GetIoTRepositoryRequest.cs
+++ IPMS.Business/Requests/IoTComponent/GetIoTRepositoryRequest.cs
@@ -2,21 +2,33 @@
 using AutoFilterer.Enums;
 using AutoFilterer.Types;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Text.Json.Serialization;
 
 namespace IPMS.Business.Requests.IoTComponent
 {
     public class GetIoTRepositoryRequest : FilterBase
     {
-        [ToLowerContainsComparison]
-        [StringFilterOptions(StringFilterOption.Contains, StringComparison.InvariantCultureIgnoreCase)]
-        [FromQuery(Name = " searchValue")]
-        public string? Name { get; set; }
+        public IoTComponentFilter Component => new IoTComponentFilter
+        {
+            SearchValue = Name
+        };
 
+        public string? Name { get; set; }
         [IgnoreFilter]
         [FromQuery(Name = "page")]
         public int Page { get; set; } = 1;
         [IgnoreFilter]
         [FromQuery(Name = "pageSize")]
         public int PageSize { get; set; } = 10;
+    }
+
+    public class IoTComponentFilter : FilterBase
+    {
+        [CompareTo(nameof(DataAccess.Models.IoTComponent.Name), nameof(DataAccess.Models.IoTComponent.Description), CombineWith = CombineType.Or)]
+        [ToLowerContainsComparison]
+        [StringFilterOptions(StringFilterOption.Contains, StringComparison.InvariantCultureIgnoreCase)]
+        [BindNever]
+        public string? SearchValue { get; init; }
     }
 }
